e98e8f05b275bb9b1bd3e2f84908d633abc94065
jeffersonroth <jjrothfilho@gmail.com>
2025-04-22 00:02:25 +0200

    grid blocks

--- src/test/test_grid_blocks.f90
+++ src/test/test_grid_blocks.f90
@@ -30,57 +30,57 @@ contains
 
         ! Define expected blocks in 3x3 format
         ! Bottom row
-        expected_blocks(1,3,:,:) = reshape([ &
+        expected_blocks(3,1,:,:) = reshape([ &  ! BL
             0, 7, 3, &
             6, 2, 4, &
             1, 5, 9  &
         ], [3,3])  ! BL
 
-        expected_blocks(2,3,:,:) = reshape([ &
+        expected_blocks(3,2,:,:) = reshape([ &  ! BM
             0, 6, 4, &
             5, 1, 9, &
             7, 8, 3  &
         ], [3,3])  ! BM
 
-        expected_blocks(3,3,:,:) = reshape([ &
+        expected_blocks(3,3,:,:) = reshape([ &  ! BR
             5, 1, 0, &
             7, 8, 3, &
             0, 6, 4  &
         ], [3,3])  ! BR
 
         ! Middle row
-        expected_blocks(1,2,:,:) = reshape([ &
+        expected_blocks(2,1,:,:) = reshape([ &  ! ML
             2, 9, 6, &
             7, 0, 8, &
             5, 3, 1  &
         ], [3,3])  ! ML
 
-        expected_blocks(2,2,:,:) = reshape([ &
+        expected_blocks(2,2,:,:) = reshape([ &  ! MM
             3, 5, 1, &
             9, 2, 0, &
             4, 0, 8  &
         ], [3,3])  ! MM
 
-        expected_blocks(3,2,:,:) = reshape([ &
+        expected_blocks(2,3,:,:) = reshape([ &  ! MR
             4, 7, 0, &
             3, 5, 1, &
             9, 2, 6  &
         ], [3,3])  ! MR
 
         ! Top row
-        expected_blocks(1,1,:,:) = reshape([ &
+        expected_blocks(1,1,:,:) = reshape([ &  ! TL
             4, 6, 7, &
             9, 1, 2, &
             0, 8, 5  &
         ], [3,3])  ! TL
 
-        expected_blocks(2,1,:,:) = reshape([ &
+        expected_blocks(1,2,:,:) = reshape([ &  ! TM
             1, 0, 0, &
             8, 3, 5, &
             6, 4, 7  &
         ], [3,3])  ! TM
 
-        expected_blocks(3,1,:,:) = reshape([ &
+        expected_blocks(1,3,:,:) = reshape([ &  ! TR
             8, 0, 5, &
             6, 0, 7, &
             1, 9, 2  &
